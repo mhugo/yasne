fcdf3b4d9911ff134c4f33add4fba24ce292fda9
Hugo Mercier <hugo@ternet.fr>
2013-10-01 20:00:55 +0200

    Begin unit test

--- main.cpp
+++ main.cpp
@@ -12,7 +12,14 @@ int main( int argc, char *argv[] )
 {
     iNESHeader header;
 
-    std::ifstream nesFile( argv[1] );
+    if ( argc < 3 ) {
+        std::cerr << "Arguments: nes_file log_file" << std::endl;
+        return 1;
+    }
+
+    std::string nesFilePath = argv[1];
+    std::string logFilePath = argv[2];
+    std::ifstream nesFile( nesFilePath.c_str() );
 
     nesFile.read( (char*)&header, sizeof( header ) );
 
@@ -29,6 +36,10 @@ int main( int argc, char *argv[] )
     cpu.pc = baseAddr;
     cpu.sp = 0xFD;
     cpu.status = 0x24;
+
+    // compare to log file
+    std::ifstream logFile( logFilePath.c_str() );
+
     //    while ( pc < memory + MEM_SIZE ) {
     for ( int i = 0; i < 50; i++ ) {
         const uint8_t* pt = (const uint8_t*)(memory + cpu.pc);
@@ -40,5 +51,27 @@ int main( int argc, char *argv[] )
         std::cout << std::endl;
         cpu.pc += instr.nOperands + 1;
         cpu.execute( instr );
+
+        // log line
+        std::string line;
+        std::getline( logFile, line );
+
+        {
+            // line
+            // address on 4 characters
+            // cpu state starting at character 48
+            std::string log_pc_str = line.substr(0,4);
+            std::string reg_a_str = line.substr(50,2);
+            std::string reg_x_str = line.substr(55,2);
+            std::string reg_y_str = line.substr(60,2);
+            std::string reg_p_str = line.substr(65,2);
+            unsigned int addr, regA, regX, regY, regP;
+            sscanf( log_pc_str.c_str(), "%04X", &addr );
+            sscanf( reg_a_str.c_str(), "%04X", &regA );
+            sscanf( reg_x_str.c_str(), "%04X", &regX );
+            sscanf( reg_y_str.c_str(), "%04X", &regY );
+            sscanf( reg_p_str.c_str(), "%04X", &regP );
+            std::cout << addr << " " << reg_a_str << " " << reg_x_str << " " << reg_y_str << " " << reg_p_str << std::endl;
+        }
     }
 }
