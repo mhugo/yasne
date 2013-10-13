3a913698d2b7122c795e6bf8e3fcf0b2ed90aa56
Hugo Mercier <hugo@ternet.fr>
2013-10-13 18:07:05 +0200

    Test mode is now optional

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ int main( int argc, char *argv[] )
 {
     iNESHeader header;
 
-    if ( argc < 3 ) {
-        std::cerr << "Arguments: nes_file log_file" << std::endl;
+    if ( argc < 2 ) {
+        std::cerr << "Arguments: nes_file [log_file]" << std::endl;
         return 1;
     }
+    bool testMode = argc > 2;
 
     std::string nesFilePath = argv[1];
-    std::string logFilePath = argv[2];
     std::ifstream nesFile( nesFilePath.c_str() );
 
     nesFile.read( (char*)&header, sizeof( header ) );
@@ -46,32 +46,40 @@ int main( int argc, char *argv[] )
 
     //    cpu.addWriteWatch( 0 );
 
-    //    memory[0x180] = 0x33;
-
     // compare to log file
-    std::ifstream logFile( logFilePath.c_str() );
+    std::ifstream logFile;
+    if ( testMode ) {
+        std::string logFilePath = argv[2];
+        logFile.open( logFilePath.c_str() );
+    }
 
     while ( true ) {
-        // log line
-        std::string line;
-        std::getline( logFile, line );
-
-        // line
-        // address on 4 characters
-        // cpu state starting at character 48
-        std::string log_pc_str = line.substr(0,4);
-        std::string reg_a_str = line.substr(50,2);
-        std::string reg_x_str = line.substr(55,2);
-        std::string reg_y_str = line.substr(60,2);
-        std::string reg_p_str = line.substr(65,2);
-        std::string reg_sp_str = line.substr(71,2);
+        // expected processor state in testMode
         unsigned int addr, regA, regX, regY, regP, regSP;
-        sscanf( log_pc_str.c_str(), "%04X", &addr );
-        sscanf( reg_a_str.c_str(), "%04X", &regA );
-        sscanf( reg_x_str.c_str(), "%04X", &regX );
-        sscanf( reg_y_str.c_str(), "%04X", &regY );
-        sscanf( reg_p_str.c_str(), "%04X", &regP );
-        sscanf( reg_sp_str.c_str(), "%04X", &regSP );
+        if ( testMode ) {
+            // log line
+            std::string line;
+            std::getline( logFile, line );
+            if ( logFile.eof() ) {
+                break;
+            }
+            
+            // line
+            // address on 4 characters
+            // cpu state starting at character 48
+            std::string log_pc_str = line.substr(0,4);
+            std::string reg_a_str = line.substr(50,2);
+            std::string reg_x_str = line.substr(55,2);
+            std::string reg_y_str = line.substr(60,2);
+            std::string reg_p_str = line.substr(65,2);
+            std::string reg_sp_str = line.substr(71,2);
+            sscanf( log_pc_str.c_str(), "%04X", &addr );
+            sscanf( reg_a_str.c_str(), "%04X", &regA );
+            sscanf( reg_x_str.c_str(), "%04X", &regX );
+            sscanf( reg_y_str.c_str(), "%04X", &regY );
+            sscanf( reg_p_str.c_str(), "%04X", &regP );
+            sscanf( reg_sp_str.c_str(), "%04X", &regSP );
+        }
 
         const uint8_t* pt = (const uint8_t*)(memory + cpu.pc);
         Instruction instr = Instruction::decode( pt );
@@ -82,15 +90,17 @@ int main( int argc, char *argv[] )
 
         printf("\tA:%02X X:%02X Y:%02X P:%02X SP:%02X\n", cpu.regA, cpu.regX, cpu.regY, cpu.status, cpu.sp );
 
-        if ( (cpu_pc != addr ) ||
-             (cpu.regA != regA) ||
-             (cpu.regX != regX) ||
-             (cpu.regY != regY) ||
-             (cpu.status != regP ) ||
-             (cpu.sp != regSP )) {
-            printf("Wrong status!\n");
-            printf("Expected: %04X A:%02X X:%02X Y:%02X P:%02X SP:%02X\n", addr, regA, regX, regY, regP, regSP );
-            break;
+        if ( testMode ) {
+            if ( (cpu_pc != addr ) ||
+                 (cpu.regA != regA) ||
+                 (cpu.regX != regX) ||
+                 (cpu.regY != regY) ||
+                 (cpu.status != regP ) ||
+                 (cpu.sp != regSP )) {
+                printf("Wrong status!\n");
+                printf("Expected: %04X A:%02X X:%02X Y:%02X P:%02X SP:%02X\n", addr, regA, regX, regY, regP, regSP );
+                break;
+            }
         }
         
 
@@ -111,6 +121,7 @@ int main( int argc, char *argv[] )
             std::cin.get();
         }
     }
+    std::cout << "End" << std::endl;
 
     return 0;
 }
